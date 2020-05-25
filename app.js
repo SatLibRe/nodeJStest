86140b82358f05d995355ce2fac7011a382d9d76
Miles Higbie <miles.higbie@gmail.com>
2020-05-25 18:45:26 -0400

    set up sample server with two routes

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const Logger = require('./logger.js')
 const path = require('path')
 const os = require('os')
 const fs = require('fs')
+const http = require('http')
 
 const logger = new Logger()
-
 
 logger.on('messageLogged', event => {
     console.log('Listener Called', event)
 })
 
+const server = http.createServer((request, response) => {
+    if(request.url === '/')   {
+        response.write('Hello Miles')
+        response.end()
+    } 
 
-logger.log("miles")
+    if(request.url == '/api/courses') {
+        response.write(JSON.stringify([1,2,3]))
+        response.end()
+    }
+})
+
+
+server.listen(3000)
+
+console.log('Listening on port 3000')
+
