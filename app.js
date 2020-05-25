7cc3ec21d33801c18adc8eda943a9455a52e7924
Miles Higbie <miles.higbie@gmail.com>
2020-05-25 14:19:00 -0400

    module exports

---
+++ app.js
@@ -0,0 +1,9 @@
+let logger = require('./logger.js')
+
+const sayHello = (name) => {
+    console.log("Hello " + name)
+}
+
+logger.log("hello")
+
+
---
+++ logger.js
@@ -0,0 +1,8 @@
+let url = 'http://fakeendpoint.com'
+
+const log = (message) => {
+    ///mock htpp request 
+    console.log(message)
+}
+
+module.exports.log = log 
